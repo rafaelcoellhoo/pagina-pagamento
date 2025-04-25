989c5eefd13a7181a772539e7bd3134663884d63
rafaelcoellhoo <rafaelcoellhoo@gmail.com>
2025-04-25 11:47:57 -0300

    Início Pagina

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import CardFront from "./components/FrontCard";
+
+
+export default function App(){
+  return(
+    <div className="w-full h-screen flex">
+      <div className="w-[40%] relative h-full bg-[#271540]">
+        <div className="absolute top-8 left-65 ">
+        <CardFront />   
+        </div>
+      </div>
+      <div className="w-[60%] h-full flex justify-end p-[40px]">
+        <h1 className="text-[45px] w-[70%] h-[150px] font-bold">Preencha os campos para concluir o pagamento! </h1>
+      </div>
+    </div>
+  )
+} 
