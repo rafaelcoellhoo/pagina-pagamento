6745e453569c8cef96402f112f37d56bb37bb052
rafaelcoellhoo <rafaelcoellhoo@gmail.com>
2025-04-28 11:57:27 -0300

    Conclusão

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,108 @@
+import { useState } from "react";
+import CardBack from "./components/BackCard";
 import CardFront from "./components/FrontCard";
 
+export default function App() {
+  const [nome, setNome] = useState("");
+  const [numero, setNumero] = useState("");
+  const [mes, setMes] = useState(0);
+  const [ano, setAno] = useState(0);
+  const [cvv, setCvv] = useState(0);
+  const [senha, setSenha] = useState("");
 
-export default function App(){
-  return(
+  function pagar() {
+    console.log(nome);
+    console.log(numero);
+    console.log(mes);
+    console.log(ano);
+    console.log(cvv);
+    console.log(senha);
+  }
+
+  return (
     <div className="w-full h-screen flex">
       <div className="w-[40%] relative h-full bg-[#271540]">
         <div className="absolute top-8 left-65 ">
-        <CardFront />   
+          <CardFront />
+        </div>
+        <div className="absolute top-80 left-96">
+          <CardBack />
         </div>
       </div>
-      <div className="w-[60%] h-full flex justify-end p-[40px]">
-        <h1 className="text-[45px] w-[70%] h-[150px] font-bold">Preencha os campos para concluir o pagamento! </h1>
+      <div className="w-[60%] h-full flex items-end p-[40px] flex-col">
+        <h1 className="text-[45px] w-[70%] h-[150px] font-bold">
+          Preencha os campos para concluir o pagamento!{" "}
+        </h1>
+        <div className="w-[65%] h-auto min-h-[200px] flex flex-col gap-4">
+          <div className="w-full flex flex-col">
+            <label htmlFor="nome" className="text-[18px]">
+              Nome no Cartão
+            </label>
+            <input
+              onChange={(event) => setNome(event.target.value)}
+              type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+            />
+          </div>
+          <div className="w-full flex flex-col">
+            <label htmlFor="numero" className="text-[18px]">
+              Número do Cartão
+            </label>
+            <input
+              onChange={(event) => setNumero(event.target.value)}
+              type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+            />
+          </div>
+          <div className="flex">
+            <div className="w-[70%] flex flex-col">
+              <label htmlFor="" className="text-[18px]">
+                Data de Expiração
+              </label>
+              <div className="w-full flex gap-2">
+                <input
+                  onChange={(event) => setMes(event.target.value)}
+                  type="number"
+                  placeholder="MM"
+                  className="w-[48%] h-[40px] pl-2 rounded-md bg-[#D9D9D9]"
+                />
+                <input
+                  onChange={(event) => setAno(event.target.value)}
+                  type="number"
+                  placeholder="AA"
+                  className="w-[48%] h-[40px] pl-2 rounded-md bg-[#D9D9D9]"
+                />
+              </div>
+            </div>
+            <div className="w-[30%] flex flex-col">
+              <label htmlFor="" className="text-[18px]">
+                CVV
+              </label>
+              <input
+                onChange={(event) => setCvv(event.target.value)}
+                type="number"
+                placeholder="CVV"
+                className="w-full h-[40px] pl-2 rounded-md bg-[#D9D9D9]"
+              />
+            </div>
+          </div>
+          <div className="w-full flex flex-col">
+            <label htmlFor="" className="text-[18px]">
+              Senha do Cartão
+            </label>
+            <input
+              onChange={(event) => setSenha(event.target.value)}
+              type="password"
+              className="w-full h-[40px] pl-2 rounded-md bg-[#D9D9D9]"
+            />
+          </div>
+          <button
+          onClick={pagar} 
+          className="w-full h-[50px] cursor-pointer rounded-md bg-[#271540] text-white font-bold">
+            Pagar
+          </button>
+        </div>
       </div>
     </div>
-  )
-} 
+  );
+}
--- src/components/BackCard/index.jsx
+++ src/components/BackCard/index.jsx
@@ -1,3 +1,16 @@
 export default function CardBack(){
-    return
+    return (
+        <div className="w-[450px] h-[250px] bg-black rounded-xl">
+            <div className="w-full h-[25%] pt-6">
+                <div className="w-full h-[50px] bg-[#2f2f2f]"></div>
+            </div>
+            <div className="w-full h-[75%] flex justify-center pt-10">
+                <div className="w-[340px] h-[50px] bg-[#AEB6BF]">
+                    <div className="text-black flex items-center justify-end mr-2 pt-2.5">
+                        <p>000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
